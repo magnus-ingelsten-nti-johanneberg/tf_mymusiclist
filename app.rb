7d6dd6c4e17f70f54ac4897eb263cba7ede3758b
magnus-ingelsten-nti-johanneberg <magnus.raaholtingelsten@elev.ga.ntig.se>
2024-02-19 14:49:22 +0100

    vecka 8
    arbete

--- app.rb
+++ app.rb
@@ -1,3 +1,16 @@
 require 'sinatra'
+require 'sinatra/reloader'
 require 'slim'
 require 'sqlite3'
+
+get('/') do
+    slim(:index)
+end
+
+get('/album/new') do
+    slim(:"album/new")
+end
+
+post('/album/new') do
+    slim(:"album/new")
+end
