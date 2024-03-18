fb3f34675d44961dd15db333aa7035c2d05bd53c
magnus-ingelsten-nti-johanneberg <magnus.raaholtingelsten@elev.ga.ntig.se>
2024-03-18 14:54:54 +0100

    vecka 12

--- app.rb
+++ app.rb
@@ -38,7 +38,7 @@ post('/albums') do
 
         db.execute("INSERT INTO artist_album_rel (artist_id, album_id) VALUES (?, ?)", new_artist_id, new_album_id)
     end
-    redirect('/')
+    redirect('/albums')
 end
 
 get('/albums') do
@@ -47,5 +47,27 @@ get('/albums') do
     all_alb = db.execute("SELECT * FROM album")
     all_art = db.execute("SELECT * FROM artist")
     all_alb_art_rel = db.execute("SELECT * FROM artist_album_rel")
-    slim(:"albums/browse",locals:{album_list:all_alb, artist_list:all_art, relation_list:all_alb_art_rel})
+    slim(:"albums/index",locals:{album_list:all_alb, artist_list:all_art, relation_list:all_alb_art_rel})
+end
+
+post('/albums/:id/delete') do
+    album_id = params[:id].to_i
+    db = SQLite3::Database.new("db/mml_db.db")
+    db.execute("DELETE FROM album WHERE id = ?", album_id)
+    db.execute("DELETE FROM artist_album_rel WHERE album_id = ?", album_id)
+    db.execute("DELETE FROM user_album_rel WHERE album_id = ?", album_id)
+    redirect('/albums')
+end
+
+get('/albums/:id/edit') do
+    album_id = params[:id].to_i
+    db = SQLite3::Database.new("db/mml_db.db")
+    db.results_as_hash = true
+    album_hash = (db.execute("SELECT title, type, release_date, name, artist_id, album_id, artist_album_rel.id FROM album INNER JOIN artist_album_rel ON album.id = artist_album_rel.album_id INNER JOIN artist ON artist.id = artist_album_rel.artist_id WHERE album_id = ?", album_id))[0]
+    
+    p "aaaa"
+    p db
+    p album_hash
+
+    slim(:"albums/edit", locals:{id:album_id, album_hash:album_hash})
 end
